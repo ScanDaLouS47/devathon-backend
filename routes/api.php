8c021ec1c4a361ffe511f94106473044b7d6f0e2
mbeas2021 <maicojobs@gmail.com>
2024-07-25 22:29:36 -0300

    Table Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TableController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,8 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::resource('/user', UserController::class);
+Route::resource('/table', TableController::class);
+
+Route::get('/table_available', [TableController::class,'available']);
+
+
