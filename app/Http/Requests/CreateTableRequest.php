af16ff8148bd729125cc718bc169458b2e174680
mbeas2021 <maicojobs@gmail.com>
2024-07-27 17:05:35 -0300

    agregando available en tables controller

--- app/Http/Requests/CreateTableRequest.php
+++ app/Http/Requests/CreateTableRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
 class CreateTableRequest extends FormRequest
@@ -24,8 +25,14 @@ class CreateTableRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'number' => 'required|integer|unique:tables,number',    
+        return [              
+            'number' => [
+                'required',
+                'integer',
+                Rule::unique('tables')->where(function ($query) {
+                    return $query->where('status', 'active');
+                }),
+            ],
             'size' => 'required|integer',
             'img' => 'nullable|string',
             'status' => 'nullable|string'        
