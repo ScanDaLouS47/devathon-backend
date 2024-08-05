5f666fb1bf26c0eb5fa5d980af5f2b3aa80138b2
josepingp <josepingp@gmail.com>
2024-08-05 14:49:18 +0200

    Refactor: Add admin user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create(['role_id' => 2]);
+        User::create([
+            'email' => 'dirij75152@maxturns.com',
+            'name' => 'Abc',
+            'lName' => 'Def',
+            'phone' => '+34123456123',
+            'status' => 'active',
+            'password' => 'f6b9ee3f-67cd-4f61-a7ef-c82b0de85660',
+            'role_id' => 2
+        ]);
 
         User::factory(10)->create(['role_id' => 1]);
     }
