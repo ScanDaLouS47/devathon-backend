f8ad1f7cb65cfcb29525c0fc83c12e47472ef40a
Marcos Lambir Torres <markos_laviana@yahoo.es>
2024-08-26 12:11:32 +0200

    feat: modify UpdateUserRequest

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -24,8 +24,8 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => "required|string|regex:/^[A-Za-zÀ-ÖØ-öø-ÿĀ-ž' ]{3,50}$/",
             'lName' => "required|string|regex:/^[A-Za-zÀ-ÖØ-öø-ÿĀ-ž' ]{3,50}$/",
-            'email' => 'required|email|unique:users,email,' . $user->id,
-            'phone' => 'required|string|regex:/^\+?[1-9]\d{1,14}$/|unique:users,phone,' . $user->id,
+            'email' => 'required|email',
+            'phone' => 'required|string|regex:/^\+?[1-9]\d{1,14}$/',
             'file' => ['nullable', ImageFile::image()->max(3 * 1024)]
         ];
     }
